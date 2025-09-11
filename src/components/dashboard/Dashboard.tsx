a56a279b6da25380cb686d2260d1fb825567f141
Andrew <59518852+akoppong@users.noreply.github.com>
2025-09-11 19:06:56 +0000

    Generated by Spark: Fix all reported errors.

--- src/components/dashboard/Dashboard.tsx
+++ src/components/dashboard/Dashboard.tsx
@@ -37,8 +37,8 @@ import { PharmaceuticalKPITemplates } from './PharmaceuticalKPITemplates';
 import { RoleTestingDashboard } from './RoleTestingDashboard';
 import { RoleShowcase } from './RoleShowcase';
 import { ResponsiveTestSuite } from '../testing/ResponsiveTestSuite';
-import { FloatingAutoFix } from '../testing/FloatingAutoFix';
 import { ResponsiveValidationSuite } from '../testing/ResponsiveValidationSuite';
+import { FloatingAutoFix } from '../testing/FloatingAutoFix';
 import { ResponsiveDesignRecommendations } from '../testing/ResponsiveDesignRecommendations';
 import { AutomatedResponsiveAnalyzer } from '../testing/AutomatedResponsiveAnalyzer';
 
